d2fb35a78082c67bd7200dba9593495dcf443e6a
shuklaaryan367-byte <shuklaaryan367@gmail.com>
2025-09-14 14:00:43 +0530

    adding cloudinary and Multer in the set-up

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,21 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_SECRET_KEY
+});
+
+const uploadCloud = async(filePath)=>{
+    try {
+        if(!filePath)return null;
+        const response =  await cloudinary.uploader.upload(filePath, {
+        resource_type:'auto'
+})
+return response;
+    } catch (error) {
+        fs.unlinkSync(filePath);
+        return null;
+    }
+}
